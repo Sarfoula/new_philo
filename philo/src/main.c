325f91b00f9b04e993e485daedaedff3f07d8b21
yann <yannallo@outlook.com>
2023-11-25 16:37:06 +0100

    resolving leaks

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: yallo <yallo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:04:12 by yallo             #+#    #+#             */
-/*   Updated: 2023/11/24 16:25:01 by yallo            ###   ########.fr       */
+/*   Updated: 2023/11/25 16:36:42 by yallo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int	main(int argc, char **argv)
 {
 	t_data			*data;
 
-	data = malloc(sizeof(t_data));
-	if (!data)
-		return (write(2, "Error: malloc failed\n", 22), 0);
 	if (argc != 5 && argc != 6)
 		return (write(2, "Error: Wrong number of arguments\n", 33), 0);
 	if (check_args(argc, argv) == 1)
 		return (0);
+	data = malloc(sizeof(t_data));
+	if (!data)
+		return (write(2, "Error: malloc failed\n", 22), 0);
 	if (init(data, argv) == 1)
 		return (0);
 	create_threads(data);
